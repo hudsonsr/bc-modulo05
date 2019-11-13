51478d6c4b644eb72e8d138b192b6ac6f7671e08
Hudson Simões Ribeiro <HudsonS@tvriosul.com.br>
2019-11-13 17:47:45 -0300

    issues List

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ApiGitHub from '../../service/ApiGitHub';
 
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 import Container from '../../components/container';
 
 // import { Container } from './styles';
@@ -64,6 +64,20 @@ class Repository extends Component {
                <h1>{repository.name}</h1>
                <p>{repository.description}</p>
             </Owner>
+            <IssueList>
+               {issues.map(issue => (
+                  <li key={String(issue.id)}>
+                     <img src={issue.user.avatar_url} alt={issue.user.login} />
+                     <div>
+                        <strong>
+                           <a href={issue.html_url}>{issue.title}</a>
+                           {/** LABELS */}
+                        </strong>
+                        <p>{issue.user.login}</p>
+                     </div>
+                  </li>
+               ))}
+            </IssueList>
          </Container>
       );
    }
